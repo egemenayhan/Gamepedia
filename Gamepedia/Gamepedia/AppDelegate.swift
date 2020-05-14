8b57e35d26a845a43db3215f7fb0c7cbb03d20ab
Egemen Ayhan <egemenayhan@icloud.com>
2020-05-14 12:18:50 +0300

    Initialize xcode project

---
+++ Gamepedia/Gamepedia/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  Gamepedia
+//
+//  Created by Egemen Ayhan on 14.05.2020.
+//  Copyright © 2020 Egemen Ayhan. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
---
+++ Gamepedia/Gamepedia/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Gamepedia
+//
+//  Created by Egemen Ayhan on 14.05.2020.
+//  Copyright © 2020 Egemen Ayhan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
+
