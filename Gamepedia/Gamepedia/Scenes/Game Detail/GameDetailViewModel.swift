29a2cbf17b5c248044226d525eb5d3686d4f8f24
Egemen Ayhan <egemenayhan@icloud.com>
2020-05-18 14:42:27 +0300

    Add favorite notification handling to game detail page

--- Gamepedia/Gamepedia/Scenes/Game Detail/GameDetailViewModel.swift
+++ Gamepedia/Gamepedia/Scenes/Game Detail/GameDetailViewModel.swift
@@ -39,6 +39,16 @@ class GameDetailViewModel {
 
     init(gameID: Int, game: Game? = nil) {
         state = GameDetailState(gameID: gameID, game: game)
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(favoriteStateUpdatedForGame(_:)),
+            name: .favoriteStateUpdatedNotification,
+            object: nil
+        )
+    }
+
+    deinit {
+        NotificationCenter.default.removeObserver(self)
     }
 
     func addChangeHandler(handler: StateChangehandler?) {
@@ -64,6 +74,13 @@ class GameDetailViewModel {
     func toggleFavoriteState() {
         guard let game = state.game else { return }
         UserDefaultsManager.shared.toggleFavoriteState(for: game)
+        stateChangeHandler?(.favoriteStateUpdated)
+    }
+
+    @objc func favoriteStateUpdatedForGame(_ notification: Notification) {
+        guard let gameID = notification.userInfo?[Global.NotificationInfoKeys.gameID] as? Int,
+            gameID == state.gameID else { return }
+
         stateChangeHandler?(.favoriteStateUpdated)
     }
 
